b0cf881ef8692368b9b88e6288ddd2c1c52c84fe
rebelsre <rebelsre@gmail.com>
2018-04-19 12:01:19 +0800

    修改 ping timeout 时间为1s，优化程序运行时间。

--- ping.sh
+++ ping.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-# Program:
-#       This script is used to detects the IP address of LAN activity.
-# History:
-#       2017/08/09 Holmes-GQB First release
+# 脚本说明:
+#       这个脚本用来检测 Class C 网络中活动的 IP 地址。
+# 修改历史:
+#       2017/08/09 Holmes-GQB 第一次发布
+#       2018/04/19 Holmes-GQB 第一次修改: 修改 ping timeout 时间为1s，优化程序运行时间。
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 export PATH
 
 for x in `seq 1 254`
-    do {
-        ping -c 1 192.168.$x.1 > /dev/null 2>&1
-        if [ $? -eq 0 ] ; then
-            for y in `seq 1 254`
-                do {
-                    ping -c 1 192.168.$x.$y > /dev/null 2>&1	#When the 192.168.[1-254].1 ping pass, to ping 192.168.[1-254].[1-254]
-                    if [ $? -eq 0 ] ; then
-                        echo "192.168.$x.$y UP"
-                    else
-                        echo "192.168.$x.$y DOWN"
-                    fi
-                }&  #Second Ping operations, background execution
-                done
-            wait    #Wait until the background check is complete
-        else
-            echo "192.168.$x.1 DOWN"
-        fi
-    }&
-    done
+do {
+    if ping -c 1 -W 1 192.168.$x.1 > /dev/null 2>&1 ; then
+        for y in `seq 1 254`
+        do {
+            if ping -c 1 -W 1 192.168.$x.$y > /dev/null 2>&1 ; then     # 当 192.168.$x.1 存活时，ping 192.168.$x.$y
+                echo "192.168.$x.$y UP"
+            else
+                echo "192.168.$x.$y DOWN"
+            fi
+        }&  # 后台执行
+        done
+        wait    # 等待后台的 ping 程序跑完
+    else
+        echo "192.168.$x.1 DOWN"
+    fi
+}&
+done
 wait
